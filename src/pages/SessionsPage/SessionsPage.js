0101124d0a5fac4ecae8817205086e8ad92c972a
kauandemello97@gmail.com <kauandemello97@gmail.com>
2023-03-12 02:59:39 -0300

    footer url e texto ok

--- src/pages/SessionsPage/SessionsPage.js
+++ src/pages/SessionsPage/SessionsPage.js
@@ -38,10 +38,10 @@ export default function SessionsPage() {
 
             <FooterContainer>
                 <div>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster" />
+                    <img src={filmes.posterURL} alt="poster" />
                 </div>
                 <div>
-                    <p>Tudo em todo lugar ao mesmo tempo</p>
+                    <p>{filmes.overview}</p>
                 </div>
             </FooterContainer>
 
